7911ad46f3c432efc0031bd0a4b6fe271a87f82c
Saif-Rahman-801 <saifurs102@gmail.com>
2023-12-22 13:44:40 +0600

    initial set up

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("HEllO WORLD");
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
