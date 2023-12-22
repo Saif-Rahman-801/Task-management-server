b5805b832701147b62b68921e41882bc1c8d137d
Saif-Rahman-801 <saifurs102@gmail.com>
2023-12-22 17:39:59 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      //   "https://dulcet-gnome-2e38bc.netlify.app",
-      "http://localhost:5173",
+      "https://marvelous-medovik-366b22.netlify.app",
+    //   "http://localhost:5173",
     ],
   })
 );
@@ -30,15 +30,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
